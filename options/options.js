74f71f958078d4f575f8dc25f300dcbc4ea5f1dc
Crowdbotics <team@crowdbotics.com>
2024-01-29 21:29:03 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fleetcor-routemaste-45676",
+  url: "https://fleetcor-routemaste-45676.botics.co",
+  api: "https://fleetcor-routemaste-45676.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
